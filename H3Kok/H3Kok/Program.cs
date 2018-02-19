90a12b79f2989a5a4990ee493aebbbdc5b13a21c
Şevket Umut Çakır <sucakir@pau.edu.tr>
2018-02-19 22:33:21 +0300

    19.02.2018 lab. soru

---
+++ H3Kok/H3Kok/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace H3Kok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, x1, x2;
+            Console.Write("a katsayısını girin: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("b katsayısını girin: ");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("c katsayısını girin: ");
+            c = Convert.ToDouble(Console.ReadLine());
+            x1 = (-b + Math.Sqrt(b * b - 4 * a * c)) / (2 * a);
+            x2 = (-b - Math.Sqrt(b * b - 4 * a * c)) / (2 * a);
+            Console.WriteLine("Kök 1={0}", x1);
+            Console.WriteLine("Kök 2={0}", x2);
+        }
+    }
+}
