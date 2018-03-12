4cfdf2a3eac88480378fd052e51beec6ff370a0b
caki <cakiaccounts@gmail.com>
2018-03-12 20:49:42 +0300

    12.03.2018 ikinci öğretim teorik ders

---
+++ teorik ders/Dizi2/Dizi3IO/Dizi3IO/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Dizi3IO
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			int[,] dizi = { { 1, 2 }, { 3, 4 }, { 5, 6 } };
+			for (int i = 0; i < 3; i++) {
+				for (int j = 0; j < 2; j++) {
+					Console.Write ("{0,3}", dizi [i, j]);
+				}
+				Console.WriteLine ();
+			}
+		}
+	}
+}
---
+++ teorik ders/RandomIO/RandomIO/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RandomIO
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Random rnd = new Random ();
+			Console.WriteLine (rnd.Next(10,20));
+			Console.WriteLine (rnd.Next(50));
+			Console.WriteLine (rnd.NextDouble());
+		}
+	}
+}
