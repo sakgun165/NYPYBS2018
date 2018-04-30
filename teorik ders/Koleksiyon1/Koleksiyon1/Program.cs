030a5d86ecb340cbadf44addbae0e63620024da9
Şevket Umut Çakır <sucakir@pau.edu.tr>
2018-04-30 15:43:30 +0300

    30.4.2018 teorik ders

---
+++ teorik ders/Koleksiyon1/Koleksiyon1/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+
+namespace Koleksiyon1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList al = new ArrayList();
+            Console.WriteLine(al.Capacity);
+            al.Add(6);
+            al.Add(25);
+            al.Add("Merhaba");
+            al.Add(2.5);
+            Console.WriteLine(al.Capacity);
+            al.Add("Dünya!");
+            Console.WriteLine(al.Capacity);
+            Console.WriteLine("al içindeki eleman sayısı:{0}",al.Count);
+            //foreach döngüsü ile ArrayList'e erişme
+            foreach (object eleman in al)
+            {
+                Console.WriteLine("Eleman değeri: {0}", eleman);
+            }
+            //for döngüsü ile ArrayList'e erişme
+            for (int i = 0; i < al.Count; i++)
+            {
+                Console.WriteLine(al[i]);
+            }
+            //ArrayList'den eleman silme
+            al.RemoveAt(1);
+            al.Remove("Dünya!");
+            al.Remove(2.5);
+            Console.WriteLine("Silme işleminden sonra al içeriği");
+            foreach (object eleman in al)
+                Console.WriteLine(eleman);
+            al.Add(5);
+            al.Add(36);
+            al.Add(41);
+            al.Add(55);
+            al.Add(41);
+            int deger = 41;
+            Console.WriteLine("{0} değerinin konumu: {1}", deger, al.IndexOf(deger));
+            if(al.Contains(55))
+                Console.WriteLine("55 değeri var");
+            al.Reverse();//sırayı tersine çevir
+            Console.WriteLine("Reverse çağrıldıktan sonra");
+            foreach(object eleman in al)
+                Console.WriteLine(eleman);
+            al.Remove("Merhaba");//hepsi aynı türden olmalı ki sıralanabilsin
+            //al.Sort();
+            for (int i = 0; i < al.Count; i++)
+            {
+                for (int j = i+1; j < al.Count; j++)
+                {
+                    if ((int)al[i] > (int)al[j])
+                    {
+                        object gecici = al[i];
+                        al[i] = al[j];
+                        al[j] = gecici;
+                    }
+                }
+            }
+            
+            Console.WriteLine("Sıralamadan sonra");
+            foreach (object eleman in al)
+                Console.WriteLine(eleman);
+            al.Insert(3, "deneme");
+            foreach (object eleman in al)
+                Console.WriteLine(eleman);
+
+        }
+    }
+}
---
+++ teorik ders/Koleksiyon2/Koleksiyon2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Koleksiyon2
+{
+    class Araba
+    {
+        string marka;
+        string model;
+        int modelYili;
+        public Araba(string marka, string model, int modelYili)
+        {
+            this.marka = marka;
+            this.model = model;
+            this.modelYili = modelYili;
+        }
+        //ToString metodu sınıfın metinsel temsili içindir. Standart olarak sınıfın ismini yazar. override özelliği ile standart metin temsili değiştirilir.
+        public override string ToString()
+        {
+            return String.Format("Araba: marka:{0} model:{1} yılı:{2}",marka,model,modelYili);//Arabanın marka, model ve model yılını biçimlendirerek döndürür
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> liste = new List<int>();
+            liste.Add(5);
+            liste.Add(22);
+            liste.Add(1);
+            liste.Add(40);
+            liste.Add(10);
+            liste.Add(21);
+            liste.Add(4);
+            List<string> metinListesi = new List<string>();
+            metinListesi.Add("dasdsa");
+            metinListesi.Add("hfdtfdw1");
+            for (int i = 0; i < liste.Count; i++)
+            {
+                for (int j = i+1; j < liste.Count; j++)
+                {
+                    if (liste[i] > liste[j])
+                    {
+                        int gecici = liste[i];
+                        liste[i] = liste[j];
+                        liste[j] = gecici;
+                    }
+                }
+            }
+            foreach (int deger in liste)
+            {
+                Console.WriteLine(deger);
+            }
+
+            List<Araba> arabaListesi = new List<Araba>();
+            Araba a1 = new Araba("BMW", "5.20", 2005);
+            Araba a2 = new Araba("Murat", "131", 1980);
+            Araba a3 = new Araba("Citroen", "C4", 2010);
+            arabaListesi.Add(a1);
+            arabaListesi.Add(a2);
+            arabaListesi.Add(a3);
+            arabaListesi.Add(new Araba("Audi", "A3", 2012));
+            
+            //döngü ile arabaları yazdır
+            foreach (Araba araba in arabaListesi)
+            {
+                Console.WriteLine(araba);
+            }
+        }
+    }
+}
